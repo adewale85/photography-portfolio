9a60a926e91c8b4eeb1da1c80dc072625b78ea1f
Odusola Adewale Iseoluwa <Iseoluwaodu85@gmail.com>
2026-03-29 14:54:21 -0700

    I make price page work on mobile

--- src/app/component/Prices.tsx
+++ src/app/component/Prices.tsx
@@ -5,7 +5,7 @@ function Prices() {
   return (
     <main className="Wrapper lg:px-0 px-4 py-32">
          
-        <div className='flex items-center justify-between'>
+        <div className='flex lg:flex-row flex-col items-center justify-between'>
             
          <div>
             <Image
@@ -22,7 +22,7 @@ function Prices() {
               </h1>
          <div className='flex gap-12'>
             <div className='space-y-12 '>
-         <div className='w-[557px] space-y-5'>
+         <div className='lg:w-[557px] w-full space-y-5'>
             <div className='flex items-center justify-between'>
                 <h3 className='font-open font-normal text-[25px] border-b text-white'>Portrait
                 </h3>
@@ -32,7 +32,7 @@ function Prices() {
             <p className='font-open font-normal text-17px text-white'>This includes individual, family, and group portraits, as well as headshots for business and corporate purposes.</p>
          </div>
 
-            <div className='w-[557px] space-y-5'>
+            <div className='lg:w-[557px] w-full space-y-5'>
             <div className='flex items-center justify-between'>
                 <h3 className='font-open font-normal text-[25px] border-b text-white'>Individual
                 </h3>
@@ -42,7 +42,7 @@ function Prices() {
             <p className='font-open font-normal text-17px text-white'>An individual photoshoot offers an opportunity for someone to have a professional photographer capture their unique personality and style through a series of posed and candid shots.</p>
          </div>
 
-            <div className='w-[557px] space-y-5'>
+            <div className='lg:w-[557px] w-full space-y-5'>
             <div className='flex items-center justify-between'>
                 <h3 className='font-open font-normal text-[25px] border-b text-white'>Fashion
                 </h3>
@@ -52,17 +52,17 @@ function Prices() {
             <p className='font-open font-normal text-17px text-white'>A fashion photoshoot is a creative process in which a team of professionals collaborates to produce images that showcase clothing, accessories, or a particular style.</p>
          </div>
           </div>
-         <div className='flex items-center justify-center h-[450px] w-[2px] bg-white'></div>
+         <div className='flex lg:flex-row flex-col items-center justify-center lg:h-[450px]  w-[2px] bg-white opacity-60'></div>
          </div>
         <div className='flex gap-3 items-end justify-end py-4'>
             <p className='font-open font-normal text-[17px] text-white'>Order Shooting</p>
              <Image
-                        src="/images/right-arrow1.svg"
-                        alt="Arrow"
-                        width={24}
-                        height={12}
-                        className="w-auto "
-                      />
+                src="/images/right-arrow1.svg"
+                alt="Arrow"
+                width={24}
+                height={12}
+                className="w-auto "
+                />
         </div>
         
           
